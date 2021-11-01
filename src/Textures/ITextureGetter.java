a919424f1f143d647e2b7613202ae39702799efb
tanko <tanko.tudor.20@stud.umfst.ro>
2021-11-01 21:11:37 +0200

    Edit Texture script and Edit Texture

--- src/Textures/ITextureGetter.java
+++ src/Textures/ITextureGetter.java
@@ -1,4 +1,15 @@
 package Textures;
 
+import java.awt.*;
+
 public interface ITextureGetter {
+
+    public Image getCrate_open();
+    public Image getCrate_closed();
+    public Image getWall();
+    public Image getCharacter();
+    public Image getDot();
+    public Image getTerrain_purple();
+    public Image getTerrain_red();
+
 }
--- src/Textures/TextureGetter.java
+++ src/Textures/TextureGetter.java
@@ -1,4 +1,70 @@
 package Textures;
 
-public class TextureGetter {
+import javax.swing.*;
+import java.awt.*;
+
+public class TextureGetter implements ITextureGetter {
+
+    public Image getCrate_open() {
+        try{
+            return new ImageIcon("D:\\School\\Modul 1\\OOP\\Laborator\\Proiect 3\\src\\Textures\\Texture\\Crate.png").getImage().getScaledInstance(32 ,32,Image.SCALE_DEFAULT);
+        }catch (Exception e ) {
+            System.out.println(e.getMessage());
+        }
+        return null;
+    }
+
+    public Image getCrate_closed() {
+        try{
+            return new ImageIcon("D:\\School\\Modul 1\\OOP\\Laborator\\Proiect 3\\src\\Textures\\Texture\\Crate check-0001.png").getImage().getScaledInstance(32 ,32,Image.SCALE_DEFAULT);
+        }catch (Exception e ) {
+            System.out.println(e.getMessage());
+        }
+        return null;
+    }
+
+    public Image getWall() {
+        try{
+            return new ImageIcon("D:\\School\\Modul 1\\OOP\\Laborator\\Proiect 3\\src\\Textures\\Texture\\Wall.png").getImage().getScaledInstance(32 ,32,Image.SCALE_DEFAULT);
+        }catch (Exception e ) {
+            System.out.println(e.getMessage());
+        }
+        return null;
+    }
+
+    public Image getCharacter() {
+        try{
+            return new ImageIcon("D:\\School\\Modul 1\\OOP\\Laborator\\Proiect 3\\src\\Textures\\Texture\\Extraterestru-0001.png").getImage().getScaledInstance(32 ,32,Image.SCALE_DEFAULT);
+        }catch (Exception e ) {
+            System.out.println(e.getMessage());
+        }
+        return null;
+    }
+
+    public Image getDot() {
+        try{
+            return new ImageIcon("D:\\School\\Modul 1\\OOP\\Laborator\\Proiect 3\\src\\Textures\\Texture\\dot-0001.png").getImage().getScaledInstance(32 ,32,Image.SCALE_DEFAULT);
+        }catch (Exception e ) {
+            System.out.println(e.getMessage());
+        }
+        return null;
+    }
+
+    public Image getTerrain_purple() {
+        try{
+            return new ImageIcon("D:\\School\\Modul 1\\OOP\\Laborator\\Proiect 3\\src\\Textures\\Texture\\Teren 1.png").getImage().getScaledInstance(32 ,32,Image.SCALE_DEFAULT);
+        }catch (Exception e ) {
+            System.out.println(e.getMessage());
+        }
+        return null;
+    }
+
+    public Image getTerrain_red() {
+        try{
+            return new ImageIcon("D:\\School\\Modul 1\\OOP\\Laborator\\Proiect 3\\src\\Textures\\Texture\\Teren 2.png").getImage().getScaledInstance(32 ,32,Image.SCALE_DEFAULT);
+        }catch (Exception e ) {
+            System.out.println(e.getMessage());
+        }
+        return null;
+    }
 }
