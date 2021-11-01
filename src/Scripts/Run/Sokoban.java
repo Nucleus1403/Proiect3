70c28098383fb4a80896fb4762a0ea8ba0a5f2ca
claudiu <claudiu.saimac@quadom.com>
2021-11-01 21:17:55 +0200

    Made Sokoban script singleton

--- src/Scripts/Run/Sokoban.java
+++ src/Scripts/Run/Sokoban.java
@@ -2,4 +2,20 @@ package Scripts.Run;
 
 public class Sokoban {
 
+    private static Sokoban instance=null;
+
+    public Sokoban getInstance()
+    {
+        if(instance==null)
+            return new Sokoban();
+        return instance;
+    }
+
+    public void StartGame()
+    {
+        
+    }
+
+
+
 }
